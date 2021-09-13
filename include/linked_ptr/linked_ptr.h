02aaf4ee24e1a5e344b569872668fe7b2fa89c2c
IkarusDeveloper <52773127+IkarusDeveloper@users.noreply.github.com>
2021-09-13 18:16:11 +0200

    + fixed import spaces

--- include/linked_ptr/linked_ptr.h
+++ include/linked_ptr/linked_ptr.h
@@ -1,7 +1,7 @@
-import<type_traits>;
-import<functional>;
-import<cassert>;
-import<unordered_set>;
+import <type_traits>;
+import <functional>;
+import <cassert>;
+import <unordered_set>;
 
 namespace linked {
     template <class _Ty> struct linked_default_delete {
